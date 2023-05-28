81e0ff0c70d277658610e5bc5061e0fc336288cc
polnt <pol.c.dev@gmail.com>
2023-05-28 20:45:49 +0200

    husky & prettier set up

--- src/index.ts
+++ src/index.ts
@@ -12,25 +12,25 @@ const app: Express = express();
 const port = appConfig.app.port || 3000;
 
 function main() {
-	app.use(cors());
+  app.use(cors());
 
-	app.options('*', cors());
-	app.use(express.json());
-	// app.use(express.urlencoded({ extended: true }));
-	// app.use(express.static(path.join(__dirname, 'public')));
+  app.options('*', cors());
+  app.use(express.json());
+  // app.use(express.urlencoded({ extended: true }));
+  // app.use(express.static(path.join(__dirname, 'public')));
 
-	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-	// app.use("/api", await router());
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // app.use("/api", await router());
 
-	app.get('/', (_, res) => {
-		res.send('<h1>EZ PZ LZ</h1>');
-	});
+  app.get('/', (_, res) => {
+    res.send('<h1>EZ PZ LZ</h1>');
+  });
 
-	app.listen(port, (err?: Error) => {
-		if (err) {
-			throw new Error('Erreur lors de la création du serveur');
-		}
-		console.log(`API OK, listening port: ${port}`);
-	});
+  app.listen(port, (err?: Error) => {
+    if (err) {
+      throw new Error('Erreur lors de la création du serveur');
+    }
+    console.log(`API OK, listening port: ${port}`);
+  });
 }
 main();
