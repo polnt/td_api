258009bd11320e2c37296f097c9692ddaf998226
polnt <pol.c.dev@gmail.com>
2023-06-24 22:46:19 +0200

    testing...

--- hack/run_tests.sh
+++ hack/run_tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 docker compose -f ./hack/docker-compose.dummy.yml up --attach api_test --build --abort-on-container-exit
-docker compose -f ./hack/docker-compose.dummy.yml down --volumes
+# docker compose -f ./hack/docker-compose.dummy.yml down --volumes
+
+# docker compose -f ./hack/docker-compose.dummy.yml up -d --build
+# docker compose exec api_test bash -c 'yarn test:all'
+# docker compose -f ./hack/docker-compose.dummy.yml down --volumes
