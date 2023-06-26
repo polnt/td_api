dd5fb41a9a7ad121d4d22af828d5fbb9ad54fde7
polnt <pol.c.dev@gmail.com>
2023-06-26 19:33:14 +0200

    testing environments and auto deploy on dh

---
+++ cicd/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cleanEnvironment() {
+  docker compose -f ./cicd/docker-compose.ci.yml down --volumes
+  docker rmi cicd-api_test
+}
+
+docker compose -f ./cicd/docker-compose.ci.yml up -d --build
+docker exec api_test bash -c 'yarn && yarn build --noEmit && ./cicd/wait_for_it.sh --timeout=0 --host=mysqldb_test --port=3306 -- yarn test'
+
+[ $? -ne 0 ] && cleanEnvironment && exit 1
+
+cleanEnvironment
+exit 0
