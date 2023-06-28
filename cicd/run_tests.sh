481236342d2582318523ad230add68c972866b0e
polnt <pol.c.dev@gmail.com>
2023-06-29 01:07:19 +0200

    checking out jest tests output

--- cicd/run_tests.sh
+++ cicd/run_tests.sh
@@ -5,8 +5,9 @@ cleanEnvironment() {
   docker rmi cicd-api_test
 }
 
-docker compose -f ./cicd/docker-compose.ci.yml up -d --build
-docker exec api_test bash -c './cicd/wait_for_it.sh --timeout=0 --host=mysqldb_test --port=3306 --strict -- yarn test'
+docker compose -f ./cicd/docker-compose.ci.yml up mysqldb_test -d --build
+docker compose -f ./cicd/docker-compose.ci.yml up api_test --build
+# docker exec api_test bash -c './cicd/wait_for_it.sh --timeout=0 --host=mysqldb_test --port=3306 --strict -- yarn test'
 
 rc=$?
 
