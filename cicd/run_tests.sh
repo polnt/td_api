4a2284f944d96ab58e3f22b57b84e4503ed58a52
polnt <pol.c.dev@gmail.com>
2023-06-29 12:44:34 +0200

    fix(run_tests.sh) docker compose now creates mysqldb_test and api_test containers

--- cicd/run_tests.sh
+++ cicd/run_tests.sh
@@ -5,7 +5,7 @@ cleanEnvironment() {
   docker rmi cicd-api_test
 }
 
-docker compose -f ./cicd/docker-compose.ci.yml up mysqldb_test -d --build
+docker compose -f ./cicd/docker-compose.ci.yml up -d --build
 docker exec api_test bash -c './cicd/wait_for_it.sh --timeout=0 --host=mysqldb_test --port=3306 --strict -- yarn test'
 
 rc=$?
